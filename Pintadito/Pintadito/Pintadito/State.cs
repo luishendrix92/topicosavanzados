5ba6f96d4b8811e3735869d588469bb81141f081
Luis Felipe López G <luishendrix92@gmail.com>
2017-04-24 18:07:13 -0700

    Aligned

--- Pintadito/Pintadito/Pintadito/State.cs
+++ Pintadito/Pintadito/Pintadito/State.cs
@@ -3,12 +3,12 @@ using System;
 
 namespace Pintadito {
   public static class State {
-    public static bool IsDrawing = false;
-    public static int BrushRadius = 20;
-    private static Color BrushColor = Color.FromArgb(26, Color.Black);
-    public static Brush AppBrush = new SolidBrush(BrushColor);
-    public static Brush EraseBrush = new SolidBrush(Color.White);
-    public static string CurrentImg = null;
+    public  static bool    IsDrawing   = false;
+    public  static int     BrushRadius = 20;
+    private static Color   BrushColor  = Color.FromArgb(26, Color.Black);
+    public  static Brush   AppBrush    = new SolidBrush(BrushColor);
+    public  static Brush   EraseBrush  = new SolidBrush(Color.White);
+    public  static string  CurrentImg  = null;
 
     public static void ChangeOpacity(int opacity) {
       BrushColor = Color.FromArgb(
